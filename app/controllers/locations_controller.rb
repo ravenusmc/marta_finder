f0e41359becb6471aff6aef66ac10910a4320fef
Mike Cuddy <mcuddy77@gmail.com>
2016-01-06 17:15:41 -0500

    got bus stops working

--- app/controllers/locations_controller.rb
+++ app/controllers/locations_controller.rb
@@ -1,6 +1,8 @@
 class LocationsController < ApplicationController
   before_action :set_location, only: [:show, :edit, :update, :destroy]
 
+  include ApplicationHelper
+
   # GET /locations
   # GET /locations.json
   def index
