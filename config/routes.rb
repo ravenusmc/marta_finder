a62e6ca2f47348dceaca964735fb673eaf36cbb6
ravenusmc <mcuddy77@gmail.com>
2016-01-09 17:02:45 -0500

    added a welcome controller with one action

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,13 @@
 Rails.application.routes.draw do
+  get 'welcome/index'
+
   resources :locations
 
-  root 'locations#new'
+  # root 'locations#new'
+
+  root 'welcome#index'
+
+  get 'new' => 'locations#new'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
